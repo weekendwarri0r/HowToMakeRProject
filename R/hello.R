8a13a0dd1f07f79e407b5646e92f64bea5800770
weekendwarri0r <weekend.warri0r.eng@gmail.com>
2017-11-04 10:08:09 +0900

    [update] add dontrun{} in roxygen

--- R/hello.R
+++ R/hello.R
@@ -3,8 +3,10 @@
 #' @param whom the function says "Hello" to
 #' @return chr
 #' @examples
+#' \dontrun{
 #' hello("Bob")
 #' hello("My friend")
+#' }
 
 hello <- function(name){
   print(paste("Hello, ", name, "!", sep=""))
